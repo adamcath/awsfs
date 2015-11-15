5f337ad91c8c62c8d140c53bddad88274e981045
acath <acath@appdynamics.com>
2015-11-14 18:24:29 -0800

    Better usage doc

--- mount-aws.sh
+++ mount-aws.sh
@@ -19,6 +19,10 @@ ${me} path
 Mounts a virtual device in the filesystem in which you can access
 various AWS services, treating them like directories and files.
 
+Your AWS credentials should be in your environment. The simplest way
+is to set AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY. See the AWS docs
+for other methods.
+
 path    Where awsfs should be mounted (e.g. ~/aws).
         Must be a directory that exists and you can write.
 EOF
